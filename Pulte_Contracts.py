52b50931ca9a40b170492509bea482aba5f18c75
TapatioSpice <157397198+TapatioSpice@users.noreply.github.com>
2024-01-27 22:07:54 -0800

    Update Pulte_Contracts.py

--- Pulte_Contracts.py
+++ Pulte_Contracts.py
@@ -5,12 +5,13 @@ import pandas as pd
 # Specify the GitHub raw content link to the Excel file
 GITHUB_EXCEL_LINK = "https://raw.githubusercontent.com/TapatioSpice/PulteContracts/main/PulteContracts1.xlsx"
 
-@st.cache_data()
+# Predefined password
+PASSWORD = "landscape11"
+
 def load_data():
     try:
         # Read the Excel file directly from the GitHub raw content link
         data = pd.read_excel(GITHUB_EXCEL_LINK)
-        print("Data loaded successfully")
         return data
     except Exception as e:
         st.error(f"Error loading data: {str(e)}")
@@ -22,31 +23,39 @@ def filter_data(data, community, series):
 
 # Function to create and display the GUI
 def create_gui(data):
-    try:
-        communities = data['Community'].unique()
-        print("Communities:", communities)
+    st.title("Pulte Contracts App")
 
-        community_col, series_col, button_col = st.columns([2, 2, 1])
+    # Password protection
+    password_input = st.text_input("Enter password:", type="password")
+    entered_password = password_input.lower()  # Convert to lowercase for case-insensitive comparison
+    if entered_password != PASSWORD:
+        st.warning("Incorrect password. Please enter the correct password to proceed.")
+        st.stop()
 
-        selected_community = community_col.selectbox('Select Community:', communities)
+    communities = data['Community'].unique()
 
-        series_options = data[data['Community'] == selected_community]['Series'].unique()
-        selected_series = series_col.selectbox('Select Series:', series_options)
+    community_col, series_col, button_col = st.columns([2, 2, 1])
 
-        if button_col.button('Create Table'):
+    selected_community = community_col.selectbox('Select Community:', communities)
+
+    series_options = data[data['Community'] == selected_community]['Series'].unique()
+    selected_series = series_col.selectbox('Select Series:', series_options)
+
+    if button_col.button('Create Table'):
+        try:
             if selected_community and selected_series:
                 filtered_data = filter_data(data, selected_community, selected_series)
                 show_table(filtered_data)
 
-    except Exception as e:
-        st.error(f"An error occurred: {str(e)}")
+        except Exception as e:
+            st.error(f"An error occurred: {str(e)}")
 
 # Function to create the table and display it
 def show_table(data):
     data = data.sort_values(by='Work Type')
 
     table_data = pd.pivot_table(data, values='Amount', index='Work Type', columns='Plan', aggfunc='sum', fill_value=0)
-    table_data.reset_index(inplace=True)  # Resetting the index to avoid extra column
+    table_data.reset_index(inplace=True)
 
     st.table(table_data)
 
